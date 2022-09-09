28e5b863966a868e6c45c884be6d065e9333fce6
user <bxl190001@utdallas.edu>
2022-09-09 17:01:38 -0500

    convert text into count vector

--- hw1/h1.py
+++ hw1/h1.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
+import nltk
+from os import walk
+from os.path import join
+import sys
 
-def counts():
-	return
+def main():
+	if len(sys.argv) < 2:
+		print(f'usage: {sys.argv[0]} <filename>')
+		exit(1)
 
-def exists():
-	return
+	for dirpath, dirnames, filenames in walk(sys.argv[1]):
+		data = [(join(dirpath, f), {}) for f in filenames]
+
+	for i in range(len(data)):
+		tokens = []
+		with open(data[i][0], 'r', errors='replace') as f:
+			words = f.read()
+			tokens = nltk.word_tokenize(words)
+
+		for token in tokens:
+			if token in data[i][1]:
+				data[i][1][token] += 1
+			else:
+				data[i][1][token] = 1
+
+		print(data[i])
+
+if __name__ == '__main__':
+	main()
