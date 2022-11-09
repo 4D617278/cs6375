5d76ff5f6cdd8bf1f56866e20a42bca4cbac691e
user <bxl190001@utdallas.edu>
2022-11-08 19:45:22 -0600

    use unsigned byte instead of byte: 2s complement representation causes values larger than 127 to become negative and have a different distance

--- hw3/KMeans.java
+++ hw3/KMeans.java
@@ -24,44 +24,49 @@ public class KMeans {
             System.out.println("Usage: Kmeans <input-image> <epochs>");
             return;
         } try {
-            BufferedImage originalImage = ImageIO.read(new File(args[0]));
-            // int k = Integer.parseInt(args[1]);
+            File inputFile = new File(args[0]);
+            BufferedImage originalImage = ImageIO.read(inputFile);
+            /*
+            long originalSize = inputFile.length();
             int epochs = Integer.parseInt(args[1]);
-            // BufferedImage kmeansJpg = kmeans_helper(originalImage, k);
 
             for (int i = 0; i < test.length; ++i) {
-                long[] size = new long[test.length];
-                long sum = 0;
+                double[] ratio = new double[epochs];
+                double sum = 0;
+                String filename = test[i] + "_" + args[0];
 
-                for (int e = 0; e < epochs; ++e) {
+                for (int e = 0; e < ratio.length; ++e) {
                     BufferedImage kmeansJpg = kmeans_helper(originalImage, test[i]);
 
                     ByteArrayOutputStream tmp = new ByteArrayOutputStream();
                     ImageIO.write(kmeansJpg, "jpg", tmp);
                     tmp.close();
 
-                    size[e] = tmp.size();
-                    sum += tmp.size();
+                    ratio[e] = (double)tmp.size() / originalSize;
+                    sum += ratio[e];
                         
                     if (e == epochs - 1)
-                        ImageIO.write(kmeansJpg, "jpg", new File(args[0] + i)); 
+                        ImageIO.write(kmeansJpg, "jpg", new File(filename)); 
                 }
 
                 // average
-                double avg = sum / test.length;
+                double avg = sum / ratio.length;
 
-                // variance
+                // sample variance
                 double var = 0;
-                for (int i2 = 0; i2 < epochs; ++i2)
-                    var += Math.pow((size[i2] - avg), 2);
+                for (int e = 0; e < ratio.length; ++e)
+                    var += Math.pow(ratio[e] - avg, 2);
+                var /= (ratio.length - 1);
 
-                var /= test.length;
-
-                System.out.println(test[i] + " & " + avg + " & " + var + " \\\\");
+                String row = args[0] + " & " + test[i] + " & " + avg + " & " + var + " \\\\";
+                System.out.println(row);
                 System.out.println("\\hline");
-            }
+            } 
+            */
 
-            // ImageIO.write(kmeansJpg, "jpg", new File(args[2])); 
+            int k = Integer.parseInt(args[1]);
+            BufferedImage kmeansJpg = kmeans_helper(originalImage, k);
+            ImageIO.write(kmeansJpg, "jpg", new File(args[2])); 
 
         } catch(IOException e) {
             System.out.println(e.getMessage());
@@ -97,7 +102,7 @@ public class KMeans {
         return kmeansImage;
     }
 
-    private static int min_dist_indx(double val, byte[] constant) {
+    private static int min_dist_indx(double val, char[] constant) {
         double[] dbl_constant = new double[constant.length];
         for (int i = 0; i < constant.length; ++i)
             dbl_constant[i] = constant[i];
@@ -129,11 +134,11 @@ public class KMeans {
             return;
         }
 
-        byte[][] rgba = new byte[Integer.BYTES][pixels.length];
+        char[][] rgba = new char[Integer.BYTES][pixels.length];
 
         for (int b = 0; b < rgba.length; ++b) {
             for (int i = 0; i < rgba[b].length; ++i) {
-                rgba[b][i] = (byte)((pixels[i] >> (Byte.SIZE * b)) & 0xff);
+                rgba[b][i] = (char)((pixels[i] >> (Byte.SIZE * b)) & 0xff);
             }
         }
 
@@ -177,6 +182,12 @@ public class KMeans {
             }
         }
 
+        for (int m = 0; m < means.length; ++m)
+            System.out.println(means[0][m]);
+
+        if (0 == 1)
+            return;
+
         for (int b = 0; b < means.length; ++b) {
             double prev_error, error = Double.MAX_VALUE;
 
@@ -188,8 +199,6 @@ public class KMeans {
                     for (int i = 0; i < rgba[b].length; ++i) {
                         int min_i = min_dist_indx(rgba[b][i], means[b]);
                         rgba_means[b][i] = min_i;
-                        // System.out.println("rgba: " + rgba[b][i]);
-                        // System.out.println("min mean[b]: " + means[b][rgba_means[b][i]]);
                         num_means[b][min_i] += 1;
                     }
 
@@ -201,13 +210,12 @@ public class KMeans {
                     for (int i = 0; i < k; ++i) {
                         if (num_means[b][i] != 0)
                             means[b][i] /= num_means[b][i];
-                        // System.out.println("b: " + b + " Mean: " + means[b][i]);
                     }
 
                     error = 0;
                     for (int i = 0; i < rgba[b].length; ++i)
                         error += Math.pow(means[b][rgba_means[b][i]] - rgba[b][i], 2);
-                    // System.out.println("Error: " + error);
+                    System.out.println("Error: " + error);
             } while (error < prev_error);
         }
 
@@ -220,10 +228,11 @@ public class KMeans {
             }
         }
 
-        for (int b = 0; b < rgba.length; ++b)
+        for (int b = 0; b < rgba.length; ++b) {
             for (int i = 0; i < rgba[b].length; ++i) {
-                rgba[b][i] = (byte)means[b][rgba_means[b][i]];
+                rgba[b][i] = (char)means[b][rgba_means[b][i]];
             }
+        }
 
         for (int i = 0; i < rgba[0].length; ++i) {
             pixels[i] = 0;
